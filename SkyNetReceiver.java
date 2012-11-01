f47a2a2c916be90d7a1b421f06d9360bacc178a9
ftufek <furkan.dev@gmail.com>
2012-11-01 19:37:43 -0400

    Changed from RS485 to Bluetooth

--- SkyNetReceiver.java
+++ SkyNetReceiver.java
@@ -4,6 +4,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import lejos.nxt.comm.Bluetooth;
 import lejos.nxt.comm.NXTConnection;
 import lejos.nxt.comm.RS485;
 import lejos.nxt.comm.RS485Connection;
@@ -25,8 +26,7 @@ public class SkyNetReceiver extends Thread {
 		//We're controlling the slave brick
 		//All it does is wait message from the master brick
 		//and execute commands based on these messages
-		//NXTConnection connection = Bluetooth.waitForConnection();
-		RS485Connection connection = RS485.waitForConnection(CONNECTION_TIMEOUT, NXTConnection.PACKET);
+		NXTConnection connection = Bluetooth.waitForConnection();
 		oStream = connection.openDataOutputStream();
 		DataInputStream iStream = connection.openDataInputStream();		
 
